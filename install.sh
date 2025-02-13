9a41ab825a1116e26e131bdc3fa160fa6e9da6da
Nicky Pike <nicky@MacBook-Pro.local>
2025-02-13 16:53:22 -0700

    added brew commands

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+echo >> /home/coder/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/coder/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+
 # Update Brew
 brew update
 
