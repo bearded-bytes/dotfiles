27799eb7085cb385d5bb39e214695f18c8ee8d76
Nicky Pike <nicky@MacBook-Pro.local>
2025-02-14 08:24:09 -0700

    updated brew eval in install script

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo >> /home/coder/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/coder/.bashrc
+echo >> /home/coder/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/coder/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 
