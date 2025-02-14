564f8003e5c6e067837af9b46d6b2db4aaaff46d
Nicky Pike <nicky@MacBook-Pro.local>
2025-02-14 10:36:47 -0700

    fixing gcloud cli install to skip prompts

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ echo "Installing gcloud cli..."
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update
-sudo apt-get install google-cloud-cli
+yes | sudo apt-get install google-cloud-cli
 
 # ZSH Default
 echo "Changing shell to zsh..."
